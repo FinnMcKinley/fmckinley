f023617b72c43e3ab3b1494d69c62dfa95a5fc5c
FinnMcKinley <finnmckinley@proton.me>
2023-11-15 10:11:08 +0000

    Installed Flask

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
+import sys
+from flask import Flask
+# Imports libraries
+
 DEBUG = True # Sets whether code is in development stage or not
 
 def log(s):
     """ Prints debug log (s) to screen"""
     if DEBUG:
         print(s)
+
