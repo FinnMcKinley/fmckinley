eb1cda578373a4505034759d18d6b8cd52283318
FinnMcKinley <finnmckinley@proton.me>
2023-11-16 08:08:39 +0000

    Flask now starts app

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import Flask
+from flask import Flask, render_template
 # Imports libraries
 
 DEBUG = True # Sets whether code is in development stage or not
@@ -9,4 +9,16 @@ def log(s):
     """ Prints debug log (s) to screen"""
     if DEBUG:
         print(s)
+        
+# # # # # # # # #
+# Flask Section #
+# # # # # # # # #
 
+app = Flask(__name__) # Starts Flask app
+
+@app.route("/") # Homepage
+def homepage():
+    return render_template("home.html")
+
+if __name__ == "__main__":    # Starts App
+    app.run(debug=True, host="0.0.0.0", port=5000)
